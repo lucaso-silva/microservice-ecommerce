dcaaa9552e8dfc63aa93a17442e52a3734ef2070
Lucas Oliveira <luc.osilva@gmail.com>
2025-10-01 00:52:48 -0700

    setup: (warehouse service) build config with Docker, AMQP, OpenAPI

--- warehouse/build.gradle.kts
+++ warehouse/build.gradle.kts
@@ -1,82 +1,70 @@
+import java.util.Date
+
 plugins {
 	id("java")
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "br.com.dio"
 version = "1.0-SNAPSHOT"
 
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
-dependencies {
+var mapStructVersion = "1.6.3"
 
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.mapstruct:mapstruct:$mapStructVersion")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
+
+	compileOnly("org.projectlombok:lombok")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	runtimeOnly("com.h2database:h2")
+
+	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor("org.mapstruct:mapstruct-processor:$mapStructVersion")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.amqp:spring-rabbit-test")
+
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+tasks.named<JavaExec>("bootRun"){
+	jvmArgs = listOf("-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=*:5005")
+}
 
-//import java.util.Date;
-//
-//plugins {
-//	java
-//	id("org.springframework.boot") version "3.5.6"
-//	id("io.spring.dependency-management") version "1.1.7"
-//}
-//
-//group = "com.lucas"
-//version = "0.0.1-SNAPSHOT"
-//description = "Microservice for E-commerce Management"
-//
-//java {
-//	toolchain {
-//		languageVersion = JavaLanguageVersion.of(21)
-//	}
-//}
-//
-//configurations {
-//	compileOnly {
-//		extendsFrom(configurations.annotationProcessor.get())
-//	}
-//}
-//
-//repositories {
-//	mavenCentral()
-//}
-//
-//var mapStructVersion = "1.6.3"
-//
-//dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-amqp")
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	implementation("org.springframework.boot:spring-boot-starter-web")
-//	implementation("org.mapstruct:mapstruct:$mapStructVersion")
-//	implementation("org.springdoc:springdoc-openai-starter-webmvc-ui:2.8.4")
-//
-//	compileOnly("org.projectlombok:lombok")
-//
-//	developmentOnly("org.springframework.boot:spring-boot-devtools")
-//
-//	runtimeOnly("com.h2database:h2")
-//
-//	annotationProcessor("org.mapstruct:mapstruct-processor:$mapStructVersion")
-//	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
-//	annotationProcessor("org.projectlombok:lombok")
-//
-//	testImplementation("org.springframework.boot:spring-boot-starter-test")
-//	testImplementation("org.springframework.amqp:spring-rabbit-test")
-//
-//	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-//}
-//
-//tasks.named("build"){
-//	doLast{
-//		val trigger = file("src/main/resources/trigger.txt")
-//		if(!trigger.exists()){
-//			trigger.createNewFile()
-//		}
-//		trigger.writeText(Date().time.toString())
-//	}
-//}
-//
-//tasks.withType<Test> {
-//	useJUnitPlatform()
-//}
+tasks.named("build"){
+	doLast{
+		val trigger = file("src/main/resources/trigger.txt")
+		if(!trigger.exists()){
+			trigger.createNewFile()
+		}
+		trigger.writeText(Date().time.toString())
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
