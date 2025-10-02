757d4c795e88a109bf67862997417bde1ba2af22
Lucas Oliveira <luc.osilva@gmail.com>
2025-10-01 21:43:19 -0700

    fix storefront Docker building proccess

--- storefront/src/main/java/com/lucas/storefront/config/AMQPConfig.java
+++ storefront/src/main/java/com/lucas/storefront/config/AMQPConfig.java
@@ -1,8 +1,13 @@
 package com.lucas.storefront.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,5 +23,21 @@ public class AMQPConfig {
         var rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(converter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    Queue queue(@Value("${spring.rabbitmq.queue.product-change-availability}") final String name){
+        return new Queue(name, true);
+    }
+
+    @Bean
+    DirectExchange exchange(@Value("${spring.rabbitmq.exchange.product-change-availability}") final String name){
+        return new DirectExchange(name);
+    }
+
+    @Bean
+    Binding binding(final Queue queue, final DirectExchange exchange,
+                    @Value("${spring.rabbitmq.routing-key.product-change-availability}") final String name){
+        return BindingBuilder.bind(queue).to(exchange).with(name);
     }
 }
